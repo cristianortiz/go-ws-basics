da7ef9952a07aedfebe16513d557f93dcfb26192
cristian ortiz navia <cristian.ortiznavia82@gmail.com>
2022-01-25 15:19:46 -0300

    minor changes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,5 +13,9 @@ func routes() http.Handler {
 
 	mux.Get("/", http.HandlerFunc(handlers.Home))
 	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	//to serve static content
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 	return mux
 }
